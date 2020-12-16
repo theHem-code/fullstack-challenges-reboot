03a9037007d264329bac2bb753456b67e92e56bf
Peter Hemsing <peterhemsing@gmail.com>
2020-12-16 23:36:29 +0100

    Ruby 1/5

--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
@@ -1,8 +1,24 @@
 def coach_answer(your_message)
   # TODO: return coach answer to your_message
+  if your_message.include?("?")
+    "Silly question, get dressed and go to work!"
+  elsif your_message.downcase == "i am going to work right now!"
+    ""
+  else
+    "I don't care, get dressed and go to work!"
+  end
 end
 
 def coach_answer_enhanced(your_message)
   # TODO: return coach answer to your_message, with additional custom rules of yours!
+  answer = coach_answer(your_message)
+  if answer != ""
+    if your_message == your_message.upcase
+      "I can feel your motivation! #{answer}"
+    else
+      answer
+    end
+  else
+    ""
+  end
 end
-
--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
@@ -1,2 +1,11 @@
 require_relative "coach_answer"
 # TODO: Implement the program that makes you discuss with your coach from the terminal.
+
+answer = nil
+
+until answer == ""
+  puts "What do u want to know from your coach?"
+  your_message = gets.chomp
+  answer = coach_answer_enhanced(your_message)
+  puts answer
+end
