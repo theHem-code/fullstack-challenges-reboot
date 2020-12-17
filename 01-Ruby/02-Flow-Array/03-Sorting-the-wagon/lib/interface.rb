5fdad3784dfacbba916ffde90166b8374dbee3e1
Peter Hemsing <peterhemsing@gmail.com>
2020-12-17 22:10:52 +0100

    Ruby 2/3

--- 01-Ruby/02-Flow-Array/03-Sorting-the-wagon/lib/interface.rb
+++ 01-Ruby/02-Flow-Array/03-Sorting-the-wagon/lib/interface.rb
@@ -9,3 +9,30 @@ require_relative "wagon_sort"
 
 # TODO: Then call `wagon_sort` method defined in the wagon_sort.rb
 #       file and display the sorted student list
+students = []
+
+puts "Type a student name:"
+user_input = gets.chomp
+
+while user_input != ""
+  students.push(user_input)
+  puts "Type another student name or press enter to finish:"
+  user_input = gets.chomp
+end
+sorted_students = wagon_sort(students)
+num_students = students.count
+
+def plural(num, word)
+  if num == 1
+    "#{word} "
+  else
+    "#{word}s"
+  end
+end
+
+puts "Congratulations! Your Wagon has #{students.count} #{plural(num_students, 'student')}:"
+if sorted_students.count >= 2
+  puts "#{sorted_students[0..-2].join(', ')} and #{sorted_students.last}"
+else
+  puts sorted_students.first
+end
