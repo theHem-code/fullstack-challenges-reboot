ecdd2e33beb50c48ad5a494c75432dbd2a6ebf46
Peter Hemsing <peterhemsing@gmail.com>
2020-12-20 23:09:29 +0100

    Ruby 4/2

--- 01-Ruby/04-Hash-Symbols/02-Modeling-with-Hash-part-II/lib/mc_donald.rb
+++ 01-Ruby/04-Hash-Symbols/02-Modeling-with-Hash-part-II/lib/mc_donald.rb
@@ -9,10 +9,24 @@ DISHES_CALORIES = {
   "Sprite" => 150
 }
 
+MEALS = {
+  "Happy Meal" => ["Cheese Burger", "French Fries", "Coca Cola"],
+  "Best Of Big Mac" => ["Big Mac", "French Fries", "Coca Cola"],
+  "Best Of McChicken" => ["McChicken", "Salad", "Sprite"]
+}
+
 def poor_calories_counter(burger, side, beverage)
   DISHES_CALORIES[burger] + DISHES_CALORIES[side] + DISHES_CALORIES[beverage]
 end
 
 def calories_counter(orders)
-  # TODO: return number of calories for a less constrained order
+  total = 0
+  orders.each do |order|
+    if DISHES_CALORIES.key?(order)
+      total += DISHES_CALORIES[order]
+    else
+      total += poor_calories_counter(MEALS[order][0], MEALS[order][1], MEALS[order][2])
+    end
+  end
+  return total
 end
