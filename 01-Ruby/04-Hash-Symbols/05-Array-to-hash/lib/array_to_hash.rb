4361cd1548e0bd9aee082721301afa9e5c518008
Peter Hemsing <peterhemsing@gmail.com>
2020-12-22 21:32:24 +0100

    Ruby 4/5

--- 01-Ruby/04-Hash-Symbols/05-Array-to-hash/lib/array_to_hash.rb
+++ 01-Ruby/04-Hash-Symbols/05-Array-to-hash/lib/array_to_hash.rb
@@ -1,3 +1,9 @@
 def array_to_hash(array)
   # TODO: implement the method :)
+  hash = {}
+  array.each_with_index do |item, index|
+    key = block_given? ? yield(index) : index.to_s
+    hash[key] = item
+  end
+  return hash
 end
