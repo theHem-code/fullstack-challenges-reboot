78ea8926f65720dc4cc21e4ae629599fe46b2f7b
Peter Hemsing <peterhemsing@gmail.com>
2020-12-29 11:41:44 +0100

    Ruby reboot 3

--- 01-Ruby/05-Regular-Expressions/Reboot-03-Instacart/lib/interface.rb
+++ 01-Ruby/05-Regular-Expressions/Reboot-03-Instacart/lib/interface.rb
@@ -1,1 +1,50 @@
 # TODO: you can build your instacart program here!
+store = {
+  "kiwi" => 1.25,
+  "banana" => 0.5,
+  "mango" => 4,
+  "asparagus" => 9
+}
+
+cart = []
+count = []
+
+# 1. Print Welcome
+puts "------------------------------"
+puts "Welcome to Peter's supermarket"
+
+# 2. Define your store (what items are for sale?) Iterate over the hash of store
+puts "We have the following items in our store:"
+
+store.each do |key, value|
+  puts "#{key}: #{value}€"
+end
+
+answer = nil
+
+until answer == 'quit'
+  puts "Which item? (or 'quit' to checkout)"
+  answer = gets.chomp
+
+  if store.key?(answer) == true
+    cart << answer
+    puts "You have #{cart.join(', ')} in your basket."
+  else
+    puts "Sorry, we don't have #{answer} today.."
+  end
+
+  puts "How many?"
+  many = gets.chomp
+  count << many
+end
+
+puts "-------BILL---------"
+
+sum = 0
+cart.each do |item|
+  sum += store[item]
+end
+puts "You need to pay #{sum}€"
+puts "--------------------"
+# 3. Get items from the user (shopping)
+# 4. Print the bill (checkout)
