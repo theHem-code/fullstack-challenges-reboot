a8d8ae67f3034283c738d3628a773b7d8aae9b56
Peter Hemsing <peterhemsing@gmail.com>
2021-01-07 11:14:20 +0100

    OOP 2/2

--- 02-OOP/02-OO-Advanced/02-Dessert-inheritance/lib/dessert.rb
+++ 02-OOP/02-OO-Advanced/02-Dessert-inheritance/lib/dessert.rb
@@ -1,15 +1,29 @@
 class Dessert
+  attr_reader :name, :calories
+
   def initialize(name, calories)
+    @name = name
+    @calories = calories
   end
 
   def healthy?
+    @calories < 200
   end
 
   def delicious?
+    return true
   end
 end
 
 class JellyBean < Dessert
+  attr_reader :flavor
+
   def initialize(name, calories, flavor)
+    super(name, calories)
+    @flavor = flavor
+  end
+
+  def delicious?
+    @flavor == "black licorice" ? false : super
   end
 end
