42693ebc43041be3ac68d373f123bad01e49ad6a
Peter Hemsing <peterhemsing@gmail.com>
2021-01-08 11:14:44 +0100

    OOP 2 optional 3

--- 02-OOP/02-OO-Advanced/Optional-03-Tracking-Subclasses/lib/mother.rb
+++ 02-OOP/02-OO-Advanced/Optional-03-Tracking-Subclasses/lib/mother.rb
@@ -1,5 +1,23 @@
 class Mother
+  def self.phone_kids
+    Daughter.phoned = true
+    Son.phoned = true
+    RandomClass.phoned = true
+  end
 end
 
 # Daughter and Son are classes inheriting from Mother. They are defined
 # in the specs, no need to create them.
+
+# class Mother
+#   @subclasses = []
+#   def self.inherited(subclass)
+#     @subclasses << subclass
+#   end
+
+#   def self.phone_kids
+#     @subclasses.each do |subclass|
+#       subclass.phone
+#     end
+#   end
+# end
