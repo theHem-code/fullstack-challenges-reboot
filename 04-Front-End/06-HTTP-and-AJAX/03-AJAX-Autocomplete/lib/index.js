c704da9b5a93b6cc136b33e5e3e06711fa8cd7f3
Peter Hemsing <peterhemsing@gmail.com>
2021-05-10 17:24:01 +0200

    words function with API

--- 04-Front-End/06-HTTP-and-AJAX/03-AJAX-Autocomplete/lib/index.js
+++ 04-Front-End/06-HTTP-and-AJAX/03-AJAX-Autocomplete/lib/index.js
@@ -1,1 +1,26 @@
 // TODO: Autocomplete the input with AJAX calls.
+
+const search = document.getElementById("search");
+const results = document.getElementById("results");
+
+const wordSuggestions = (keyword) => {
+  const url = `https://wagon-dictionary.herokuapp.com/autocomplete/${keyword}`;
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      data.words.forEach((word) => {
+        const wordList = `<li>
+            <p><i class="fas fa-search"></i>${word}</p>
+            </li>`;
+        results.insertAdjacentHTML("beforeend", wordList);
+      });
+    });
+};
+
+search.addEventListener("keyup", (event) => {
+  event.preventDefault();
+  const search = document.getElementById("search");
+  results.innerHTML = "";
+  wordSuggestions(search.value);
+});
