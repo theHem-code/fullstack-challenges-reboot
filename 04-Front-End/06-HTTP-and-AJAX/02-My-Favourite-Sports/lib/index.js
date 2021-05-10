9965effc17f1e11c856dbb3c66b848fb6a023ef1
Peter Hemsing <peterhemsing@gmail.com>
2021-05-10 16:37:19 +0200

    multiple choices

--- 04-Front-End/06-HTTP-and-AJAX/02-My-Favourite-Sports/lib/index.js
+++ 04-Front-End/06-HTTP-and-AJAX/02-My-Favourite-Sports/lib/index.js
@@ -1,1 +1,27 @@
 // TODO: write your code here!
+
+const buttons = document.querySelectorAll(".clickable");
+
+const toggleActiveClass = (event) => {
+  event.currentTarget.classList.toggle("active");
+};
+
+// REFACTOR:
+// const toggleActiveOnClick = (button) => {
+//   button.addEventListener("click", toggleActiveClass);
+// };
+
+// buttons.forEach(toggleActiveOnClick);
+
+
+buttons.forEach((button) => {
+  button.addEventListener("click", toggleActiveClass);
+});
+
+
+// 'classList' returns a live DOMTokenList collection of the class attributes of the element.
+// This can then be used to manipulate the class list.
+
+// explanation....
+// if visible is set remove it, otherwise add it
+// div.classList.toggle("visible");
