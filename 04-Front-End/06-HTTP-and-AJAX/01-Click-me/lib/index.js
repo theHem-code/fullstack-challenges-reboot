c174d5d0d1c1e5693c6ec408f67198690052e81c
Peter Hemsing <peterhemsing@gmail.com>
2021-05-10 15:59:40 +0200

    button challange

--- 04-Front-End/06-HTTP-and-AJAX/01-Click-me/lib/index.js
+++ 04-Front-End/06-HTTP-and-AJAX/01-Click-me/lib/index.js
@@ -1,7 +1,11 @@
 const displayAlertOnButtonClick = () => {
   // TODO: Select the big green button
+  const button = document.getElementById("btn");
 
   // TODO: Bind the `click` event to the button
+  button.addEventListener("click", (event) => {
+    alert("Thank you!");
+  });
   // TODO: On click, display `Thank you!` in a JavaScript alert!
 };
 
