f0bf786e3157ec45a27967ba2095bf68da2f6dc5
Peter Hemsing <peterhemsing@gmail.com>
2021-05-11 11:42:55 +0200

    bugfixes

--- 04-Front-End/07-JavaScript-Plugins/01-Bugfix/lib/logo.js
+++ 04-Front-End/07-JavaScript-Plugins/01-Bugfix/lib/logo.js
@@ -1,4 +1,6 @@
 const displayLogo = () => {
-  const logo = document.querySelector('#logo');
-  logo.style.display = '';
+  const logo = document.querySelector("#logo");
+  logo.style.display = "";
 };
+
+export { displayLogo };
